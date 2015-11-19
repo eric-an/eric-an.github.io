d9af67a57734c48abf7de484beec2c8e31ff4500
Eric An <erican@me.com>
2015-11-19 03:59:04 -0800

    update links, layout, and colors

--- js/index.js
+++ js/index.js
@@ -23,10 +23,10 @@ for (var i = 0; i < skillType.length; i++) {
   $("#" + i).css("background", "url(" + skillType[i].pic + ")");
 };
 
-$(".skill_item").mouseenter(function () {
-  $(this).html("<p class='experience_info'><img src=" + skillType[this.id].experience + " width='90' height='70'></p>");
-}).mouseleave(function () {
-  $(".experience_info").html("");
-});
+// $(".skill_item").mouseenter(function () {
+//   $(this).html("<p class='experience_info'><img src=" + skillType[this.id].experience + " width='90' height='70'></p>");
+// }).mouseleave(function () {
+//   $(".experience_info").html("");
+// });
 
 
